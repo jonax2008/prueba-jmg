82fd81a2cc8584c63a26cdb2a1c8ef5bc9e6aee6
jonax2008 <jmg_619@hotmail.com>
2025-03-08 08:32:34 -0600

    Add Name ValueObject

---
+++ app/Domain/User/ValueObject/Name.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Domain\User\ValueObject;
+
+use InvalidArgumentException;
+
+final class Name {
+    private string $value;
+    const MIN_LENGHT = 3;
+
+    public function __construct(string $value) {
+        $this->validate($value);
+        $this->value = $value;
+    }
+
+    public function getValue(): string {
+        return $this->value;
+    }
+
+    public function validate($value): void {
+        $this->validateMinLength($value);
+        $this->validateChars($value);
+    }
+
+    public function validateMinLength($value): void {
+        if (strlen($value) < self::MIN_LENGHT) {
+            throw new InvalidArgumentException('Invalid length. Minimun required is ' . self::MIN_LENGHT);
+        }
+    }
+
+    public function validateChars($value): void {
+        if (!preg_match('/^[a-zA-Z\s]+$/', $value)) {
+            throw new InvalidArgumentException('Invalid characters. Only letters and spaces are allowed.');
+        }
+    }
+
+    public function equals(Name $other): bool {
+        return $this->value === $other->getValue();
+    }
+}
