4130f19deadb3d4306d3936f0ffcca000978f134
jonax2008 <jmg_619@hotmail.com>
2025-03-08 08:38:45 -0600

    Add Email ValueObject

---
+++ app/Domain/User/ValueObject/Email.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Domain\User\ValueObject;
+
+use InvalidArgumentException;
+
+final class Email {
+    private string $value;
+
+    public function __construct(string $value) {
+        $this->validate($value);
+        $this->value = $value;
+    }
+
+    public function getValue(): string {
+        return $this->value;
+    }
+
+    public function validate(string $value): void {
+        $this->validateEmailFormat($value);
+    }
+
+    public function validateEmailFormat($value): void {
+        if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
+            throw new InvalidArgumentException("Invalid email format: $value");
+        }
+    }
+
+    public function equals(Name $other): bool {
+        return $this->value === $other->getValue();
+    }
+}
