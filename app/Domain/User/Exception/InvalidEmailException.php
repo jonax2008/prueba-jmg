7d9395a122b79149320e372a7a34b477f230ca44
jonax2008 <jmg_619@hotmail.com>
2025-03-08 08:49:36 -0600

    Format InvalidEmailException

--- app/Domain/User/Exception/InvalidEmailException.php
+++ app/Domain/User/Exception/InvalidEmailException.php
@@ -4,10 +4,8 @@ namespace App\Domain\User\Exception;
 
 use Exception;
 
-class InvalidEmailException extends Exception
-{
-    public function __construct(string $email)
-    {
+class InvalidEmailException extends Exception {
+    public function __construct(string $email) {
         parent::__construct("Invalid email format: $email");
     }
 }
